205025c4d53bad61219504f138302b16e6e5285f
Bueno, Feliphe <feliphezion@gmail.com>
2017-01-02 11:39:29 -0300

    De acordo com a issue #12
    Signed-off-by: Bueno, Feliphe <feliphezion@gmail.com>

--- phpaccess.php
+++ phpaccess.php
@@ -9,13 +9,13 @@ if($fileDir[0] === 'bin'){
     $appRoot = \realpath(__DIR__ . '../../../../');
 }
 
-if (\preg_match('/\.(?:png|jpg|jpeg|gif|txt)$/', $_SERVER["REQUEST_URI"])) {
+if (\preg_match('/\.(?:png|jpg|jpeg|gif|txt)$/', \getenv('REQUEST_URI'))) {
     return false;
 } else {
-    $exp = \explode('/', $_SERVER["REQUEST_URI"]);
+    $exp = \explode('/', \getenv('REQUEST_URI'));
     if(isset($exp[1])){
         $app    = $exp[1];
-        $q      = \preg_replace('|\/'. $exp[1] .'\/|', '', $_SERVER["REQUEST_URI"]);
+        $q      = \preg_replace('|\/'. $exp[1] .'\/|', '', \getenv('REQUEST_URI'));
         $_GET['q'] = $q;
         include $appRoot . \DIRECTORY_SEPARATOR .'index.php';
     } else {
